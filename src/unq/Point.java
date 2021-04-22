0fa1897596a1408edd6aab08c46e739f18298279
Ariel <Ariel@192.168.0.101>
2021-04-22 15:56:40 -0300

    punto 6

---
+++ src/unq/Point.java
@@ -0,0 +1,42 @@
+package unq;
+
+public class Point {
+	private int x;
+	private int y;
+	
+	public Point() {
+		x=0;
+		y=0;
+	}
+	
+	public Point(int i, int j) {
+		this.setXY(i,j);
+	}
+
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+
+	public void setXY(int i, int j) {
+		this.setX(i);
+		this.setY(j);
+	}
+
+	private void setY(int j) {
+		y = j; 
+	}
+
+	private void setX(int i) {
+		x = i;
+	}
+
+	public Point sumarCon(Point p) {
+		Point nuevoPunto = new Point(this.getX()+p.getX(), this.getY()+p.getY());
+		return nuevoPunto;
+	}
+	
+}
---
+++ test/unq/PointTestCase.java
@@ -0,0 +1,52 @@
+package unq;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PointTestCase {
+	 
+	private Point punto1;
+	private Point punto2;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		punto1 = new Point();
+		punto2 = new Point(1,2);
+	}
+	
+	@Test
+	void testConstructorConParametros() {
+		int x = punto2.getX();
+		int y = punto2.getY();
+		assertEquals(1,x);
+		assertEquals(2,y);
+	}
+
+	@Test
+	void testConstructorSinParametros() {
+		int x = punto1.getX();
+		int y = punto1.getY();
+		assertEquals(0, x);
+		assertEquals(0, y);	
+	}
+	
+	@Test
+	void testMoverPunto() {
+		punto1.setXY(3,5);
+		int x = punto1.getX();
+		int y = punto1.getY();
+		assertEquals(3, x);
+		assertEquals(5, y);	
+	}
+	
+	@Test
+	void testSumarPuntos() {
+		Point punto3 = punto1.sumarCon(punto2);
+		int x = punto3.getX();
+		int y = punto3.getY();
+		assertEquals(1, x);
+		assertEquals(2, y);
+	}
+}	
