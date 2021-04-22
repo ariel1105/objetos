83e6e569b197c83765bc2a5d7623f515bc6185ac
Ariel <Ariel@192.168.0.101>
2021-04-22 14:34:30 -0300

    punto 4

---
+++ src/unq/Multioperador.java
@@ -0,0 +1,42 @@
+package unq;
+
+import java.util.ArrayList;
+
+public class Multioperador {
+	private ArrayList<Integer> elementos = new ArrayList<Integer>();
+
+	public void agregar(int i) {
+		elementos.add(i);
+	}
+
+	public int suma() {
+		int suma = 0;
+		for (int i=0; i < elementos.size(); i++) {
+			suma = suma + elementos.get(i); 
+		}
+		return suma;
+	}
+
+	public int resta() {
+		if (elementos.isEmpty()) {
+			return 0;
+		}
+		int resta = elementos.get(0);
+		for (int i = 1; i < elementos.size(); i++) {
+			resta = resta - elementos.get(i);
+		}
+		return resta;
+	}
+
+	public int multiplicacion() {
+		if (elementos.isEmpty()) {
+			return 0;
+		}
+		int multiplicacion = elementos.get(0);
+		for (int i = 1; i < elementos.size(); i++) {
+			multiplicacion = multiplicacion * elementos.get(i);
+		}
+		return multiplicacion;
+	}
+
+}
---
+++ test/unq/MultioperadorTestCase.java
@@ -0,0 +1,38 @@
+package unq;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MultioperadorTestCase {
+	private Multioperador multioperador;
+	
+	@BeforeEach
+	public void setUp() throws Exception{
+		multioperador = new Multioperador();
+		multioperador.agregar(2);
+		multioperador.agregar(10);
+		multioperador.agregar(5);
+	}
+
+	
+	@Test
+	public void testSumar() {
+		int amount = multioperador.suma();
+		assertEquals(17, amount);
+	}
+	
+	@Test
+	public void testRestar() {
+		int amount = multioperador.resta();
+		assertEquals(-13, amount);
+	}
+	
+	@Test
+	public void testMultiplicar() {
+		int amount = multioperador.multiplicacion();
+		assertEquals(100, amount);
+	}
+
+}
